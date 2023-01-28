626e7261d0b2604ccda23427fe8d8df1b2f2dc5d
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-01-28 22:11:03 +0000

    Added color-coding to tale rows depending on the current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 let currentDayEl = $("#currentDay");
 currentDayEl.text(moment().format("LLLL").split(" ").slice(0, 4).join(" "));
 
-// Table
+
+let currentHour = moment().hour();
+// Append empty rows to the table
 let table = $('#projectTable');
 
-for (let i = 9; i < 18; i++) {
+
+
+for (let i = 9; i < 24; i++) {
   let row = $("<tr>");
   let time = $("<td>");
   let task =  $("<td>");
@@ -13,13 +17,13 @@ for (let i = 9; i < 18; i++) {
   if (i < 12) {
     time.append(
       $("<p>")
-        .text(i + "am")
+        .text(i + " am")
         .css('width', '10%')
     );
   } else {
     time.append(
       $("<p>")
-        .text(i + "pm")
+        .text(i + " pm")
         .css('width', '10%')
     );
   }
@@ -30,5 +34,16 @@ for (let i = 9; i < 18; i++) {
   row.append(time);
   row.append(task);
   row.append(isCompleted);
+  if (currentHour > parseInt(time.text().split(' ')[0])) {
+    row.addClass('past');
+  }
+    else if (currentHour === parseInt(time.text().split(' ')[0])) {
+        row.addClass('present');
+    }
+    else {
+        row.addClass('future');
+    }
   table.append(row);
 }
+
+
