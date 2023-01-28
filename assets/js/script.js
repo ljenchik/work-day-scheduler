96a27d0752466f6ba1b1c63ec340e5f70da964a2
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-01-28 21:31:44 +0000

    Added rows to the table with hours and complete task button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,34 @@
-let currentDayEl= $("#currentDay");
-currentDayEl.text(moment().format('LLLL').split(' ').slice(0,4).join(' '));
+let currentDayEl = $("#currentDay");
+currentDayEl.text(moment().format("LLLL").split(" ").slice(0, 4).join(" "));
+
+// Table
+let table = $('#projectTable');
 
 for (let i = 9; i < 18; i++) {
-    if (i < 12) {
-        $("#time").append($("<p>").text(i + 'am').addClass('p-2'));
-    }
-    else {
-        $("#time").append($("<p>").text(i + 'pm').addClass('p-2'));
-    }
-    $("#task").append($("<p>").addClass('p-2'));
+  let row = $("<tr>");
+  let time = $("<td>");
+  let task =  $("<td>");
+  let isCompleted =  $("<td>");
 
-    let icon = $("<i>").addClass("fa-solid fa-calendar-check");
-    $("isCompleted").append($("<p>"));
+  if (i < 12) {
+    time.append(
+      $("<p>")
+        .text(i + "am")
+        .css('width', '10%')
+    );
+  } else {
+    time.append(
+      $("<p>")
+        .text(i + "pm")
+        .css('width', '10%')
+    );
+  }
+  
+  task.append($("<p>")).css('width', '80%');
+  isCompleted.append('<i class="fa-solid fa-calendar-check fa-2xl"></i>');
+
+  row.append(time);
+  row.append(task);
+  row.append(isCompleted);
+  table.append(row);
 }
