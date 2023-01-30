90b19c8c6378e0b679718a5da99a3130e49ae5c0
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-01-30 22:06:22 +0000

    Added code to prevent displaying insaved tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,23 @@ let currentDayEl = $("#currentDay");
 currentDayEl.text(moment().format("LLLL").split(" ").slice(0, 4).join(" "));
 let currentHour = moment().hour();
 
+let lastClicked;
 let table = $("#projectTable");
 let timeblock;
 
-let dayTasks = {}
+let dayTasks = {};
 // Add date to clear tasks for the previous day
 dayTasks.date = moment().format("DD/MM/YYYY");
 
 function createTable() {
-  for (let i = 9; i < 24; i++) {
-    
+  for (let i = 9; i < 18; i++) {
     // Table row
     timeblock = $("<tr>").addClass("time-block");
 
     // Table cells
-    let time = $("<td>").css('background', '#06aed5').addClass('time');
+    let time = $("<td>").css("background", "#06aed5").addClass("time");
     let task = $("<td>");
-    let save = $("<td>").css('background', '#06aed5');
+    let save = $("<td>").css("background", "#06aed5");
 
     // Add hours
     if (i < 12) {
@@ -33,7 +33,10 @@ function createTable() {
     task.append(inputEl);
 
     // Add icon for saving the task
-    save.append('<i class="fa-solid fa-floppy-disk"></i>').css('background', '#06aed5').addClass('saveBtn');
+    save
+      .append('<i class="fa-solid fa-floppy-disk"></i>')
+      .css("background", "#06aed5")
+      .addClass("saveBtn");
 
     timeblock.append(time);
     timeblock.append(task);
@@ -56,11 +59,19 @@ function createTable() {
     // Add row to the table
     table.append(timeblock);
 
+    inputEl.on("click", function () {
+      if (lastClicked && lastClicked.type === "text" && lastClicked !== this) {
+        // To prevent displaying if save button is not clicked
+        getTasks();
+      }
+      lastClicked = this;
+    });
+
     // Save task to array of objects
-    
     save.on("click", function () {
+      lastClicked = this;
       let text = $(this).parent().children().children("input").val();
-    
+
       let hour = time.text().split(" ")[0];
       // Add to array if task is not empty
       if (text) {
@@ -71,27 +82,25 @@ function createTable() {
         delete dayTasks[hour];
       }
       localStorage.setItem("tasks", JSON.stringify(dayTasks));
+      // Prevent from displaying unsaved task
+      getTasks();
     });
   }
 }
-
-
 
 function getTasks() {
   if (JSON.parse(localStorage.getItem("tasks"))) {
     dayTasks = JSON.parse(localStorage.getItem("tasks"));
   }
-  console.log("dayTasks", dayTasks);
 
-  for (const [key, value] of Object.entries(dayTasks)) {
-    // Compare key from dayTasks array to data-index of each row which represents time to 
-    // determine which row to fill with the task in
-    $('#projectTable > tbody > tr').each(function() { 
-      if (`${key}` ===  $(this).attr('data-index')) {
-        $(this).children().children('input').val(`${value}`);
-      };
-});
-    
+  for (let key = 9; key < 18; key++) {
+    let value = dayTasks[key.toString()] || "";
+
+    $("#projectTable > tbody > tr").each(function () {
+      if (`${key}` === $(this).attr("data-index")) {
+        $(this).children().children("input").val(`${value}`);
+      }
+    });
   }
 
   // Clear local storage from the previous day tasks
@@ -99,7 +108,6 @@ function getTasks() {
     localStorage.clear();
   }
 }
-
 
 createTable();
 getTasks();
